9a023ce2b9d39dafd947561265ab44cec718d334
Subhadeep-CS <dassubhadeep631@gmail.com>
2024-07-14 01:07:56 +0530

    Configure Tailwind-CSS 14/07/2024

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-
+      <p className='w-7'>Hello Movie application</p>
     </div>
   );
 }
