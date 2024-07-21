84050af0ef19aa6964c11c0183b2ee27630f8a8e
Subhadeep-CS <dassubhadeep631@gmail.com>
2024-07-21 23:21:28 +0530

    Signin/Signup features completed

--- src/Layout/AppLayout.js
+++ src/Layout/AppLayout.js
@@ -1,22 +1,37 @@
 import Home from "../pages/Home";
 import Browse from "../pages/Browse";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-const AppLayout=()=>{
-    const appRouter=createBrowserRouter([
-        {
-            path:'/',
-            element:<Home/>,
-        },
-        {
-            path:'/browse',
-            element:<Browse/>
-        }
-    ])
-    return(
-        <div>
-            <RouterProvider router={appRouter}></RouterProvider>
-        </div>
-    );
-}
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, Bounce } from "react-toastify";
+const AppLayout = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+  return (
+    <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
+      <RouterProvider router={appRouter}></RouterProvider>
+    </div>
+  );
+};
 
 export default AppLayout;
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,68 @@
 import { useFormik } from "formik";
 import { useState } from "react";
 import { signinSchema, signupSchema } from "../utils/formValidationSchema";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebase";
+import { toast,Bounce } from "react-toastify";
 
 const LoginForm = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
 
-  const { handleChange, handleBlur, handleSubmit, touched, values, errors } = useFormik({
-    initialValues: isSignInForm
-      ? { email: "", password: "" }
-      : { name: "", email: "", password: "", confirm_password: "" },
-    validationSchema: isSignInForm ? signinSchema : signupSchema,
-    enableReinitialize: true,
-    onSubmit: (values,action) => {
-      if(isSignInForm){
-        //sign in logic goes here
-      }
-      else{
-        //signup logic goes here
-      }
-    },
-  });
+  const { handleChange, handleBlur, handleSubmit, touched, values, errors } =
+    useFormik({
+      initialValues: isSignInForm
+        ? { email: "", password: "" }
+        : { name: "", email: "", password: "", confirm_password: "" },
+      validationSchema: isSignInForm ? signinSchema : signupSchema,
+      enableReinitialize: true,
+      onSubmit: (values, action) => {
+        if (isSignInForm) {
+            const {email,password}=values;
+          //sign in logic goes here
+          signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+              // Signed in
+              const user = userCredential.user;
+              console.log(user);
+              toast.success("Login Successfully");
+            })
+            .catch((error) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              toast.error(errorMessage);
+            });
+        } 
+        else 
+        {
+          //signup logic goes here
+          const { email, password } = values;
+          createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+              // Signed up
+              const user = userCredential.user;
+              console.log(user);
+              const { providerData } = user;
+              toast.success(`${providerData.email} registered successfully`);
+            })
+            .catch((error) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              console.log(errorCode + "" + errorMessage);
+            });
+        }
+        action.resetForm();
+      },
+    });
 
   return (
-    <div id="loginForm" className="relative flex justify-center items-center min-h-screen">
-      <form className="p-8 w-full max-w-md bg-black bg-opacity-80 text-white rounded-md" onSubmit={handleSubmit}>
+    <div
+      id="loginForm"
+      className="relative flex justify-center items-center min-h-screen"
+    >
+      <form
+        className="p-8 w-full max-w-md bg-black bg-opacity-80 text-white rounded-md"
+        onSubmit={handleSubmit}
+      >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -40,7 +79,9 @@ const LoginForm = () => {
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
-              {touched.name && errors.name && <p className="text-red-700 font-bold">{errors.name}</p>}
+              {touched.name && errors.name && (
+                <p className="text-red-700 font-bold">{errors.name}</p>
+              )}
             </>
           )}
         </div>
@@ -55,7 +96,9 @@ const LoginForm = () => {
             onChange={handleChange}
             onBlur={handleBlur}
           />
-          {touched.email && errors.email && <p className="text-red-700 font-bold">{errors.email}</p>}
+          {touched.email && errors.email && (
+            <p className="text-red-700 font-bold">{errors.email}</p>
+          )}
         </div>
         <div>
           <input
@@ -68,7 +111,9 @@ const LoginForm = () => {
             onChange={handleChange}
             onBlur={handleBlur}
           />
-          {touched.password && errors.password && <p className="text-red-700 font-bold">{errors.password}</p>}
+          {touched.password && errors.password && (
+            <p className="text-red-700 font-bold">{errors.password}</p>
+          )}
         </div>
         <div>
           {!isSignInForm && (
@@ -83,17 +128,29 @@ const LoginForm = () => {
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
-              {touched.confirm_password && errors.confirm_password && <p className="text-red-700 font-bold">{errors.confirm_password}</p>}
+              {touched.confirm_password && errors.confirm_password && (
+                <p className="text-red-700 font-bold">
+                  {errors.confirm_password}
+                </p>
+              )}
             </>
           )}
         </div>
         <div>
-          <button type="submit" className="block w-full p-4 my-6 rounded-md bg-red-700">
+          <button
+            type="submit"
+            className="block w-full p-4 my-6 rounded-md bg-red-700"
+          >
             {isSignInForm ? "Sign In" : "Sign Up"}
           </button>
         </div>
-        <p className="py-2 font-semibold cursor-pointer" onClick={() => setIsSignInForm(!isSignInForm)}>
-          {isSignInForm ? "New to Netflix? Sign Up Now" : "Existing User? Log In Now"}
+        <p
+          className="py-2 font-semibold cursor-pointer"
+          onClick={() => setIsSignInForm(!isSignInForm)}
+        >
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Existing User? Log In Now"}
         </p>
       </form>
     </div>
