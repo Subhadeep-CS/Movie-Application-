cbb79be121423ae38b87b7f8408916ab6d4fe137
Subhadeep-CS <dassubhadeep631@gmail.com>
2024-08-17 19:52:06 +0530

    Bug Fixed

--- src/Layout/AppLayout.js
+++ src/Layout/AppLayout.js
@@ -3,49 +3,29 @@ import Browse from "../pages/Browse";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, Bounce } from "react-toastify";
-import { auth } from "../config/firebase";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../redux/slice/userSlice";
 import Header from "./Header";
 const AppLayout = () => {
-  const dispatch=useDispatch();
   const appRouter = createBrowserRouter([
     {
       path: "/",
       element: (
         <>
-          <Header/>
+          <Header />
           <Home />
         </>
       ),
     },
     {
       path: "/browse",
-      element:(
+      element: (
         <>
-          <Header/>
+          <Header />
           <Browse />
         </>
       ),
     },
   ]);
 
-  //onAuthState change called only once app level
-  useEffect(()=>{
-    onAuthStateChanged(auth,(user)=>{
-      if(user){
-        //user signin
-        const {uid,email,displayName,photoURL}=user;
-        dispatch(addUser({uid,email,displayName,photoURL}));
-      } 
-      else{
-        //user signout
-        dispatch(removeUser());
-      }
-    })
-  },[])
   return (
     <div>
       <ToastContainer
--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -2,9 +2,33 @@ import Logo from "../assets/images/logo.png";
 import UserIcon from "../assets/images/profile_icon.png";
 import ProfileDropdown from "../components/ProfileDropdown";
 import { useSelector } from "react-redux";
+import { auth } from "../config/firebase";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { addUser, removeUser } from "../redux/slice/userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
   const user = useSelector((store) => store.user);
   console.log(user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  //onAuthState change called only once app level
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        //user signin
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({ uid, email, displayName, photoURL }));
+        navigate("/browse");
+      } else {
+        //user signout
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+  }, []);
+
   return (
     <>
       <div
